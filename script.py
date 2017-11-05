ed288ea119f1e4a77cd2aed31d63022ab27ed34b
Bash <Bashir_45@hotmail.com>
2017-11-05 16:23:44 -0500

    updated python script

--- script.py
+++ script.py
@@ -1,11 +1,5 @@
-import webbrowser
-import time
 print ("Hello World")
 time.sleep(2)
-print ("Now you are taking off")
-time.sleep(3)
-print ("See you in the browser")
-time.sleep(2)
-url = "https://facebook.com"
+url = "https://github.com/"
 webbrowser.open(url)
 
